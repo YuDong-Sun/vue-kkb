c97555292ecc62c03fcf64c593589bfccfab93b4
YuDong-Sun <a_sunyudong@163.com>
2020-01-19 17:09:41 +0800

    first commit

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,38 +1,60 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
     <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
+      <li v-for="item in goods" v-bind:key="item.id">
+        <span>商品:{{item.text}}</span>
+        <span>價格:{{item.price}}</span>
+        <button @click="addCar(item)">加入購物車</button>
+      </li>
     </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+    <input type="text" v-model="text">
+    <button @click="addProduct">添加</button>
+    <h2>{{ text }}</h2>
+    {{cart}}
   </div>
 </template>
 
 <script>
 export default {
   name: 'HelloWorld',
+  data(){
+    return {
+      title:"",
+      goods:[
+        {id:1,text:'目录一',price:"666"},
+        {id:2,text:'目录二',price:"555"},
+        {id:3,text:'目录三',price:"111"},
+        {id:4,text:'目录四',price:"3685"}
+      ],
+      text:"",
+      cart:[]
+    }
+  },
+  created(){
+    setTimeout(() => {
+      this.title = "定时器title"
+    }, 3000);
+  },
+  methods:{
+    addProduct(){
+      if(this.text){
+        this.goods.push({
+          id:this.goods.length+1,
+          text:this.text,
+          price:"1356"
+        })
+      }
+      this.text = "";
+    },
+    addCar(item){
+      const ret = this.cart.find(v => v.id === item.id);
+      if(ret){
+        ret.count += 1;
+      }else{
+        this.cart.push({...item,count:1})
+      }
+    }
+  },
   props: {
     msg: String
   }
@@ -44,13 +66,8 @@ export default {
 h3 {
   margin: 40px 0 0;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
+ul li {
+  list-style:none;
 }
 a {
   color: #42b983;
